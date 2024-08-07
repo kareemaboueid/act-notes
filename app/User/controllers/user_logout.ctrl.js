395d4504f362decd4a050b31fa75e9dc9168327b
Kareem Aboueid <kareemabo3id@hotmail.com>
2024-08-08 01:18:05 +0300

    feat: Add controller for user logout functionality

---
+++ app/User/controllers/user_logout.ctrl.js
@@ -0,0 +1,27 @@
+import asyncHandler from 'express-async-handler';
+import { StatusCodes } from 'http-status-codes';
+
+/**
+ * ### Controller `ctrl_user_logout()`
+ * @access Public
+ * @method POST
+ * @route `/v1/users/logout`
+ */
+const ctrl_user_logout = asyncHandler(async (_request, _response) => {
+  try {
+    // ------ START ------ //
+
+    // send success response:
+    _response.status(StatusCodes.OK).json({ message: 'User has been logged out.' });
+
+    // ------ HANDLE ERRORS ------ //
+  } catch (_error) {
+    // send error response:
+    _response.status(StatusCodes.BAD_REQUEST);
+    throw new Error(_error.message);
+  }
+
+  // ------ END ------ //
+});
+
+export default ctrl_user_logout;
