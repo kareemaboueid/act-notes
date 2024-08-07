cad09bbe9940010b1a16350966502ad53cab1920
Kareem Aboueid <kareemabo3id@hotmail.com>
2024-08-08 01:15:38 +0300

    feat: Add NoSql note schema

---
+++ app/Note/schemas/note.schema.js
@@ -0,0 +1,63 @@
+import mongoose from 'mongoose';
+import { db_structure } from '../../../constants/db_structure.cnst.js';
+
+/** ### Note Schema */
+const note_schema = new mongoose.Schema(
+  // ------ START ------ //
+
+  {
+    NOTE_TITLE: {
+      type: String,
+      required: [true, 'Title is required'],
+      Unique: true,
+      index: true,
+      trim: true,
+    },
+
+    NOTE_CONTENT: {
+      type: String,
+      required: [true, 'Content is required'],
+      default: 'No content',
+    },
+
+    NOTE_STATUS: {
+      type: String,
+      default: 'todo',
+    },
+
+    NOTE_FOLDER: {
+      type: String,
+      default: 'home',
+    },
+
+    NOTE_IMPORTANCE: {
+      type: String,
+      default: 'normal',
+    },
+
+    FLAG_FREEZE: {
+      type: Boolean,
+      default: false,
+    },
+
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: db_structure.user.model,
+    },
+  },
+
+  {
+    timestamps: true,
+    collection: db_structure.note.coll,
+    versionKey: false,
+  },
+
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  },
+
+  // ------ END ------ //
+);
+
+export default note_schema;
