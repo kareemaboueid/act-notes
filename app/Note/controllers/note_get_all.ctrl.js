33bf3f3dc763b759ff908c34e857892e7f308f1f
Kareem Aboueid <kareemabo3id@hotmail.com>
2024-08-08 01:16:57 +0300

    feat: Add controller for getting all notes

---
+++ app/Note/controllers/note_get_all.ctrl.js
@@ -0,0 +1,41 @@
+import asyncHandler from 'express-async-handler';
+import { StatusCodes } from 'http-status-codes';
+import Note_model from '../models/note.model.js';
+import hlp_jsend from '../../../utilities/json_send.hlp.js';
+
+/**
+ * ### Note get all controller.
+ * @access Private
+ * @method GET
+ * @route `/v1/notes?all=true`
+ */
+
+const ctrl_note_get_all = asyncHandler(async (_request, _response) => {
+  try {
+    // ------ START ------ //
+
+    // database all notes:
+    const DB_ALL_NOTES = await Note_model.find().where(
+      'this.FLAG_FREEZE === false && this.NOTE_FOLDER !== "trash"',
+    );
+
+    // check if notes not exists:
+    if (!DB_ALL_NOTES) {
+      // send error response:
+      _response.status(StatusCodes.BAD_REQUEST);
+      throw new Error('No notes found');
+    }
+
+    // send success response:
+    _response.status(StatusCodes.OK).json(hlp_jsend(StatusCodes.OK, DB_ALL_NOTES));
+
+    // ------ HANDLE ERRORS ------ //
+  } catch (_error) {
+    // send error response:
+    _response.status(StatusCodes.BAD_REQUEST);
+    throw new Error(_error.message);
+  }
+
+  // ------ END ------ //
+});
+export default ctrl_note_get_all;
