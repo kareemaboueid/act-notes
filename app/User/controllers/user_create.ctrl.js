c1a2bb98c33002c6b78f24d1c4bd544f2a7a4623
Kareem Aboueid <kareemabo3id@hotmail.com>
2024-08-09 00:03:54 +0300

    feat: Refactor user creation controller and add error handling

--- app/User/controllers/user_create.ctrl.js
+++ app/User/controllers/user_create.ctrl.js
@@ -1,14 +1,15 @@
 import asyncHandler from 'express-async-handler';
 import { StatusCodes } from 'http-status-codes';
 import User_model from '../models/user.model.js';
-import hlp_jsend from '../../../utilities/json_send.hlp.js';
+import utl_jsend from '../../../utilities/json_send.utl.js';
 
 /**
- * ### Controller `ctrl_user_create()`
+ * ### ctrl_user_create
  * @access Public
  * @method POST
  * @route `/v1/users/create`
  */
+
 const ctrl_user_create = asyncHandler(async (_request, _response) => {
   try {
     // ------ START ------ //
@@ -22,37 +23,35 @@ const ctrl_user_create = asyncHandler(async (_request, _response) => {
       // confirm_password: eud_user_confirm_password,
     } = _request.body;
 
-    // check if passwords match:
-    // if (eud_user_password !== eud_user_confirm_password) {
-    //   _response.status(StatusCodes.BAD_REQUEST);
-    //   throw new Error('Passwords do not match, try again');
-    // }
-
     // database user query:
-    const USER = await User_model.findOne({
+    const DB_USER = await User_model.findOne({
       USER_USERNAME: eud_user_username,
     }).where({ FLAG_FREEZE: false });
 
     // check if user exists:
-    if (USER) {
+    if (DB_USER) {
       _response.status(StatusCodes.BAD_REQUEST);
       throw new Error('User already exists, try again');
     }
 
-    // create new user in database:
-    await User_model.create({
+    // define user payload:
+    const user_payload = {
       USER_FULLNAME: eud_user_fullname,
       USER_EMAIL: eud_user_email,
       USER_USERNAME: eud_user_username,
       USER_PASSWORD: eud_user_password,
-    });
+    };
 
-    // TODO [cookie w/JWT] save the new user info in secure httpOnly cookie w/ JWT:
+    // create new user in database:
+    await User_model.create(user_payload);
 
     // send success response:
-    _response
-      .status(StatusCodes.CREATED)
-      .json(hlp_jsend(StatusCodes.CREATED, { message: 'User created successfully' }));
+    _response.status(StatusCodes.CREATED).json(
+      utl_jsend(StatusCodes.CREATED, {
+        message: 'User created successfully',
+        username: `@${eud_user_username}`,
+      }),
+    );
 
     // ------ HANDLE ERRORS ------ //
   } catch (_error) {
