5ae76cf0d95e5781874b982eb7c62db72b376319
Kareem Aboueid <kareemabo3id@hotmail.com>
2024-08-09 03:36:00 +0300

    refactor: Update app initialization to include user and note schemas

--- index.js
+++ index.js
@@ -28,6 +28,8 @@ import database_connect from './configs/db/database_connect.cnfg.js';
 import server_listen from './configs/server/server_listen.cnfg.js';
 import { path_strings } from './constants/path_strings.cnst.js';
 import logger from './configs/logging/logger.cnfg.js';
+import user_schema from './app/User/schemas/user.schema.js';
+import note_schema from './app/Note/schemas/note.schema.js';
 
 // APP INITIALIZATION:
 const app = express();
@@ -41,9 +43,13 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(helmet());
 app.use(cookieParser());
-if (NODE_ENV === 'development') app.use(morgan('dev'));
-if (NODE_ENV === 'development') mongoose.set('debug', true);
-if (NODE_ENV === 'development') logger.env({ level: 'debug' });
+if (NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+  mongoose.set('debug', true);
+  logger.env({ level: 'debug' });
+  logger.is_schema_strict('User', note_schema.options.strict);
+  logger.is_schema_strict('Note', user_schema.options.strict);
+}
 
 // SERVER ROUTERS:
 app.use(path_strings.user.root, user_router);
